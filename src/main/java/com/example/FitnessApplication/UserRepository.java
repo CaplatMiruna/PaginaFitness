2d2114581329deddb78a793839a0d56047009b5b
Miru <mctxy05@gmail.com>
2023-12-14 07:44:09 +0200

    UserRepo

---
+++ src/main/java/com/example/FitnessApplication/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.FitnessApplication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    void deleteUserById(Long id);
+
+    Optional<User> findUserById(Long id);
+}
